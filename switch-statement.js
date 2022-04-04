e0811846b787a2a6742ff5741e8873c4777b2293
Chibu <atuchibueze@gmail.com>
2022-04-04 14:31:06 -0400

    second commit

---
+++ switch-statement.js
@@ -0,0 +1,23 @@
+const option = 5;
+
+switch (option) {
+  case 1:
+  console.log("Your option is 1");
+  break;
+  case 2:
+  console.log("Your option is 2");
+  break;
+  case 3:
+  console.log("Your option is 3");
+  break;
+  case 4:
+  console.log("Your option is 4");
+  break;
+  case 5:
+  console.log("Your option is 5");
+  break;
+  case 6:
+  console.log("Your option is 6");
+  break;
+  
+}
